b6e56db9bf4f3b58a20e6c933ba30c2f65be423b
Rasmus Turn Kruse <rasm158w@mail.hansenberg.dk>
2023-12-20 13:43:15 +0100

    for Loop

---
+++ Examples/Loop.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Genspil {
+
+static void Main()     {   
+             int int1, int2;
+Console.Write("\n\n");
+Console.Write("equal or not?");
+Console.Write("\n\n");
+Console.Write("Input nummer 1.");
+int1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input nummer 2.");
+int2 = Convert.ToInt32(Console.ReadLine());
+
+if (int1==int2)
+ Console.WriteLine("{0} and {1} are equal.\n",int1,int2);
+    else
+        Console.WriteLine("{0} and {1} are not equal.\n",int1,int2);
+
+
+                } 
+
+ }
+
