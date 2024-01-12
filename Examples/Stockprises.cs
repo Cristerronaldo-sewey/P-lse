dcb8589f76a784eeca335a67072e276649c08370
Rasmus Turn Kruse <rasm158w@mail.hansenberg.dk>
2024-01-12 14:16:48 +0100

    Int virker

--- Examples/Stockprises.cs
+++ Examples/Stockprises.cs
@@ -1,16 +1,18 @@
 using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
 
 class Stockprices{
 
     public static void Main()
     {
 
-//         string[] stocknames = { "stockA", "stockB", "stockC"};
+//         string[] stocknames = { "stockA", "stockB", "stockC", "stockD"};
 //         double[,] stockPrices = {
                         
 //                     {3,7,8,10,45},
 //                     {5,6,12,15,60},
-//                     {7,23,25,34,46}
+//                     {7,23,25,34,46},
+//                     {2,3,223,35,6}
 
 //                     };
 //         Console.WriteLine("Stock priser over tiden");
@@ -47,19 +49,39 @@ class Stockprices{
 //             }
 //         }
             
-        string[] stocknames = { "stockA", "stockB"};
-        int[] stockprice = {2,4,5};
+      
+        string[] stocknames = { "stockA", "stockB", "stockC" };
+        int[] value = { 2, 4, 5, 32, 23, 234, 2, 1, 0, 10 };
+        int[] stockprice =
+        value;
+    
 
-Console.WriteLine($"The names of the stocks: {stocknames}");
+        Console.WriteLine($"The names of the stocks: {string.Join(", ", stocknames)}");
 
-        int i = 0;
-        for (i = 0; i < stockprice.Length; i++);
+        for (int i = 0; i < stocknames.Length; i++)
+        {
+            int[] priceForStock = new int[stockprice.Length];
 
-
-
-
-
-
-
+            for (int j = 0; j < stockprice.Length; j++)
+            {
+                priceForStock[j] = stockprice[i];
             }
-       }
+
+            int averagePrice = CalculateAverage(priceForStock);
+
+            Console.WriteLine($"{stocknames[i]} - Average Price: {averagePrice}");
+        }
+    }
+
+    static int CalculateAverage(int[] prices)
+    {
+        int sum = 0;
+
+        foreach (int price in prices)
+        {
+            sum += price;
+        }
+
+        return sum / prices.Length;
+    }
+}
