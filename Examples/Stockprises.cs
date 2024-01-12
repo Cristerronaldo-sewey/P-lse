461a860f900816c88d94138ff4132832cdc73f36
Rasmus Turn Kruse <rasm158w@mail.hansenberg.dk>
2024-01-12 13:21:37 +0100

    Stockprices Double. startede på int

---
+++ Examples/Stockprises.cs
@@ -0,0 +1,65 @@
+using System.Globalization;
+
+class Stockprices{
+
+    public static void Main()
+    {
+
+//         string[] stocknames = { "stockA", "stockB", "stockC"};
+//         double[,] stockPrices = {
+                        
+//                     {3,7,8,10,45},
+//                     {5,6,12,15,60},
+//                     {7,23,25,34,46}
+
+//                     };
+//         Console.WriteLine("Stock priser over tiden");
+    
+//     //Beregningerne
+//         for(int i = 0; i < stocknames.Length; i++)
+
+// {
+//    double[] pricesForStock = new double[stockPrices.GetLength(1)];
+
+//    for(int j = 0; j < stockPrices.GetLength(1); j++)
+//     {
+//          pricesForStock[j] = stockPrices[i, j];  
+//             }
+
+//     double averagePrice = CalculateAverage(pricesForStock);
+            
+//             Console.WriteLine($"{stocknames[i]} - Average Price: {averagePrice}");
+//          }
+         
+//                     double CalculateAverage(double[] prices)
+//                     {
+//                     double sum = 0;
+
+//                     foreach (var price in prices)
+//                     {
+//                         sum += price;
+//                     }
+
+//                     return sum / prices.Length;
+                    
+                
+//                 }
+//             }
+//         }
+            
+        string[] stocknames = { "stockA", "stockB"};
+        int[] stockprice = {2,4,5};
+
+Console.WriteLine($"The names of the stocks: {stocknames}");
+
+        int i = 0;
+        for (i = 0; i < stockprice.Length; i++);
+
+
+
+
+
+
+
+            }
+       }
